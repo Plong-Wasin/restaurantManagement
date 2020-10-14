542fd1e29e14e0f8bfb7db099cae5cb7579ff0cc
Wasin Phungwigrai <wasin2541@hotmail.com>
2020-10-14 22:05:24 +0700

    :bug: Debug style

--- Staff/StaffScreen.php
+++ Staff/StaffScreen.php
@@ -11,7 +11,7 @@ include('../require/connectDB.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>พนักงาน</title>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <link rel="stylesheet" href="../../CSS/Staff.css">
+    <link rel="stylesheet" href="../CSS/Staff.css">
 </head>
 
 <body>
