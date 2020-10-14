f9a066658470cb0052b557f2c39f5ca7420dcbcd
Wasin Phungwigrai <wasin2541@hotmail.com>
2020-10-14 23:16:05 +0700

    Update bootstrap url

--- require/req.php
+++ require/req.php
@@ -1,4 +1,4 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://unpkg.com/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
